9f8189a5ec88effea209064c1c62fdaf2aead10a
Lavanyasugumar96 <lavanyasugumar.slm@gmail.com>
2024-11-21 18:54:02 +0530

    created expense tracker

---
+++ expense/src/App.js
@@ -0,0 +1,14 @@
+import Home from "./Home";
+import { SnackbarProvider } from "notistack";
+
+function App() {
+  return (
+    <SnackbarProvider>
+      <div>
+        <Home />
+      </div>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
